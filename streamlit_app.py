facb346a13515e27a1b39db12ede7c2aa4680aa1
hsaripalli <45947172+hsaripalli@users.noreply.github.com>
2023-07-30 21:36:28 -0600

    Update streamlit_app.py
    added breakfast menu header

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,18 @@ import pandas as pd
 
 st.title('New')
 st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+st.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+st.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+st.text(' 🐔 Hard-Boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-#st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+st.multiselect('Pick some fruits:', list(my_fruit_list.index))
 
 st.dataframe(my_fruit_list)
 

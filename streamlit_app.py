a762af8aad8c368044176f79b3173919dc6e7cfe
hsaripalli <45947172+hsaripalli@users.noreply.github.com>
2023-07-30 21:33:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,10 @@ st.text('Kale, Spinach & Rocket Smoothie')
 st.text('Hard-Boiled Free-Range Egg')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+#my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 st.dataframe(my_fruit_list)
 
